ee760671b2f45b3fbecc9278cbaeaa89e726ac66
AvinandanBose <boseavinandan@gmail.com>
2022-10-04 16:23:28 +0530

    Initial commit

---
+++ lib/listviewexample.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyAppPage extends StatefulWidget {
+  const MyAppPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyAppPage> createState() => _MyAppPageState();
+}
+
+class _MyAppPageState extends State<MyAppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List View Builder Example'),
+        centerTitle: true,
+      ),
+      body: Wrap(
+        direction: Axis.horizontal,
+        spacing: 30,
+        children: List.generate(3, (index) {
+          return Column(
+            children: <Widget>[
+              Container(
+                height: 30,
+                width: 60,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Center(child: Text('Hello')),
+              ),
+
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
